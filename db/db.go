c434dbdab4933f4d0b9d77c1dc1e7f1fdcf7f586
Saifullah shaheen <shahriar350@gmail.com>
2022-01-11 22:30:07 +0600

    ent setting done

--- db/db.go
+++ db/db.go
@@ -2,33 +2,27 @@ package db
 
 //
 import (
+	"bongo/ent"
+	"context"
+	"fmt"
 	_ "github.com/jackc/pgx/v4/stdlib"
 	_ "github.com/lib/pq"
+	"log"
 )
 
 //
-//var Client *ent.Client
-//var err error
+var Client *ent.Client
+var err error
 
-//func Open(databaseUrl string) *ent.Client {
-//	db, err := sql.Open("pgx", databaseUrl)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//	// Create an ent.Driver from `db`.
-//	drv := entsql.OpenDB(dialect.Postgres, db)
-//	return ent.NewClient(ent.Driver(drv))
-//}
-
-//func Init() {
-//	Client, err = ent.Open("postgres", "host=localhost port=5432 user=postgres dbname=bongobitan password=123456 sslmode=disable")
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//	fmt.Println("Connected to database successfully")
-//	//defer Client.Close()
-//	// Run the auto migration tool.
-//	if err := Client.Schema.Create(context.Background()); err != nil {
-//		log.Fatalf("failed creating schema resources: %v", err)
-//	}
-//}
+func Init() {
+	Client, err = ent.Open("postgres", "host=localhost port=5432 user=postgres dbname=bongo_ent password=123456 sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to database successfully")
+	//defer Client.Close()
+	// Run the auto migration tool.
+	if err := Client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+}
